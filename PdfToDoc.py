77c9bde1147a6edb87c1ab1f51c06754938d49db
MD RAFIUL ISLAM <per.rafiuli02@gmail.com>
2026-09-17 22:28:56 +0600

    Modify PdfToDoc.py to accept user input for file names

--- PdfToDoc.py
+++ PdfToDoc.py
@@ -1,6 +1,9 @@
 import os
 from pdf2docx import Converter
 
+pdf_file_name = input("Enter your PDF file name : ")
+doc_file_name = input("Enter Doc file name that you want to save : ")
+
 def pdf_to_docx(pdf,doc):
     if not os.path.exists(pdf):
         print("No file found.")
@@ -14,4 +17,4 @@ def pdf_to_docx(pdf,doc):
     except Exception as e:
         print(f"Error{e}")
 
-pdf_to_docx("Nibir_CV.pdf","Nibir_resume_doc.docx")
+pdf_to_docx(pdf_file_name,doc_file_name)
