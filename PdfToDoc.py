197d33d7103159837d8d099c1a6e4e924add7a48
MD RAFIUL ISLAM <per.rafiuli02@gmail.com>
2026-09-18 18:44:31 +0600

    Update PdfToDoc.py

--- PdfToDoc.py
+++ PdfToDoc.py
@@ -1,20 +1,21 @@
 import os
-from pdf2docx import Converter
+from pdf2docx import Converter  #Importing Converter class from pdf2docx library
 
-pdf_file_name = input("Enter your PDF file name : ")
-doc_file_name = input("Enter Doc file name that you want to save : ")
-
-def pdf_to_docx(pdf,doc):
-    if not os.path.exists(pdf):
+def pdf_to_docx(pdf,doc):  #function for pdf to doc converter
+    if not os.path.exists(pdf):  #Checking if the file available or not.
         print("No file found.")
-        return
+        return  #Stops the programme here if there is no file found
     
     try:
-        cv = Converter(pdf)
+        cv = Converter(pdf) #The file we want to convert.
         cv.convert(doc, start=0, end=None)
-        cv.close()
+        cv.close() #After converting the file to doc closing the Converter()
         print("Pdf converted to Doc.")
-    except Exception as e:
-        print(f"Error{e}")
 
-pdf_to_docx(pdf_file_name,doc_file_name)
+    except Exception as e:
+        print(f"Error -> {e}") #If fail to convert the file then error message will be shown.
+
+pdf_file_name = input("Enter your PDF file name : ")  
+doc_file_name = input("Enter Doc file name that you want to save : ")
+
+pdf_to_docx(f"{pdf_file_name}.pdf",f"{doc_file_name}.docx")  #calling the pdf_to_docx() converter function.
